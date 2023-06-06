02b66f2a6df2c079942439cbf22be1360d8bef0e
davit.sargsyan <d.sargsyan@teamjet.com>
2023-06-06 11:10:53 +0400

    test change 1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Header from "./copmponents/Header/Header";
 const SignIn = lazy(() => import("./copmponents/Sign-In/SignIn"));
 const SignUp = lazy(() => import("./copmponents/Sign-up/Sign-up"));
 
-console.log("test")
+console.log("test-1")
 
 
 function App() {
