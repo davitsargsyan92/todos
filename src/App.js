f96a19917c69094e848e4ea1938060d15737efac
davit.sargsyan <d.sargsyan@teamjet.com>
2023-06-06 11:17:33 +0400

    feature 1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Header from "./copmponents/Header/Header";
 const SignIn = lazy(() => import("./copmponents/Sign-In/SignIn"));
 const SignUp = lazy(() => import("./copmponents/Sign-up/Sign-up"));
 
-console.log("test-1")
+console.log("test-1 feature branch1")
 
 
 function App() {
